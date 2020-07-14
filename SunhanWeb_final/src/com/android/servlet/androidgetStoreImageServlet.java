52704b324b39f9a061f90519f946dbcd6d9fe305
leenahyeong <68145824+leenahyeong@users.noreply.github.com>
2020-07-14 22:12:12 +0900

    Update androidgetStoreImageServlet.java

--- SunhanWeb_final/src/com/android/servlet/androidgetStoreImageServlet.java
+++ SunhanWeb_final/src/com/android/servlet/androidgetStoreImageServlet.java
@@ -43,54 +43,46 @@ public class androidgetStoreImageServlet extends HttpServlet {
 		response.setCharacterEncoding("UTF-8"); response.setContentType("text/html; charset=UTF-8");
 		
 			try {
-					// 1. JDBC 드라이버 설치
-					// mysql-connector-java-8.0.11.jar 파일을 web-inf >> lib 폴더에 추가
-             
-					// 2. 설치되어있는 드라이버를 로드
-					Class.forName("com.mysql.jdbc.Driver");
+				Class.forName("com.mysql.jdbc.Driver");
 
-					// 3. 드라이버를 사용해서 디비 연결 아이디/비밀번호/디비 주소
-					// mysql-connector-java 6.XX 이상 사용시 디비 접근 주소(서버시간 인식 오류)
-					// String dbURL = "jdbc:mysql://localhost:3306/itwill";
-					String dbURL = "jdbc:mysql://3.12.173.221:3306/projectsd?&characterEncoding=UTF-8";
-					String dbID = "mysqluser";
-					String dbPW = "user123";
+				String dbURL = "*";
+				String dbID = "*";
+				String dbPW = "*";
 					 
-					Connection conn =  DriverManager.getConnection(dbURL, dbID, dbPW);
+				Connection conn =  DriverManager.getConnection(dbURL, dbID, dbPW);
              
-					ResultSet rs = null;
-					PreparedStatement pstmt=null;
-					PrintWriter outs =  response.getWriter();
+				ResultSet rs = null;
+				PreparedStatement pstmt=null;
+				PrintWriter outs =  response.getWriter();
 					
-					JSONObject store = new JSONObject();
-					JSONArray arr = new JSONArray();
+				JSONObject store = new JSONObject();
+				JSONArray arr = new JSONArray();
 					
-					String _id = new String(request.getParameter("id"));
+				String _id = new String(request.getParameter("id"));
 					
-					String SQL ="select fileRealName from storefile where userid=?";	
-					pstmt = conn.prepareStatement(SQL);
-					pstmt.setString(1, _id);
-					rs = pstmt.executeQuery();
-					while (rs.next())
-					{  
-							JSONObject obj = new JSONObject();
-							obj.put("fileRealName",rs.getString("fileRealName"));
-							arr.put(obj);
-					}
-					if(conn!=null)
-					{	
+				String SQL ="select fileRealName from storefile where userid=?";	
+				pstmt = conn.prepareStatement(SQL);
+				pstmt.setString(1, _id);
+				rs = pstmt.executeQuery();
+				while (rs.next()){  
+					JSONObject obj = new JSONObject();
+					obj.put("fileRealName",rs.getString("fileRealName"));
+					arr.put(obj);
+				}
+				if(conn!=null)
+				{	
 						
-					}
-					store.put("storeImagelist", arr);
-					outs.print(store);
+				}
+				store.put("storeImagelist", arr);
+				outs.print(store);
 			}catch(ClassNotFoundException e) {
-					// 드라이버를 불러올 때 문제가 발생
-					System.out.println("드라이버 로드 실패");
-					e.printStackTrace();
-				}catch(SQLException e) {
-					// DB 접속 실패
-					System.out.println("DB 접속 실패");
-					e.printStackTrace();
-			}
+				// 드라이버를 불러올 때 문제가 발생
+				System.out.println("드라이버 로드 실패");
+				e.printStackTrace();
+			}catch(SQLException e) {
+				// DB 접속 실패
+				System.out.println("DB 접속 실패");
+				e.printStackTrace();
+		}
 	}
 }
